4d8c6953683a448a2a9d7a56b7b12cee199d99d6
Tencaxe <30882469+Tencaxe@users.noreply.github.com>
2021-08-12 01:19:26 -0300

    Add files via upload

---
+++ Model/cartao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trello.Model
+{
+    public class cartao
+    {
+        public int idcartao{get;set;}
+        public string nome { get; set; }
+        public string  descricao { get; set; }
+
+    }
+}
